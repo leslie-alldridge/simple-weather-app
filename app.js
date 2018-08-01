295c488c64c646a11366fef803c00ade5c9f3969
Leslie Alldridge <leslie.alldridge@gmail.com>
2018-08-01 17:31:07 +1200

    Adding svg files and slight UI changes
    Adding svg files and slight UI changes

--- app.js
+++ app.js
@@ -12,57 +12,88 @@ function weather() {
       latitude = position.coords.latitude;
       longitude = position.coords.longitude;
 
-      location.innerHTML = 'Latitude is ' + latitude + '° Longitude is ' + longitude + '°';
+      location.innerHTML = 'Your latitude is ' + latitude + '° and your longitude is ' + longitude + '°';
 
        $.getJSON(url + apiKey + "/" + latitude + "," + longitude + "?callback=?", function(data) {
-        var temp = data.currently.temperature;
-        console.log(temp);
+        //get temperature in F
+        var tempF = data.currently.temperature;
+        // change to celcius 
+        tempF = parseFloat((tempF-32)/1.8).toFixed(2); 
+        //Summary says partly cloudy as an example
         var summary = data.currently.summary;
-        console.log(summary);
-
+        //gather icon data from API
         icon = data.currently.icon;
-        console.log(icon);
-
-        
+          //set variables for html fields
           var imgHome = document.getElementById("weatherIcon");
+          var tempLocation = document.getElementById("temp");
+          var resultText = document.getElementById("result");
+
           switch(icon){
             case 'clear-day':
-            imgHome.src="images/sun.svg"
+            imgHome.src="images/sun.svg";
+            tempLocation.innerHTML=("The temperature is " + tempF + " degrees celcius.");
+            resultText.innerHTML=(summary);
             break;
+
             case 'clear-night':
+            imgHome.src="images/night.svg";
+            tempLocation.innerHTML=("The temperature is " + tempF + " degrees celcius.");
+            resultText.innerHTML=(summary);
             break;
     
             case 'rain':
+            imgHome.src="images/rain.svg";
+            tempLocation.innerHTML=("The temperature is " + tempF + " degrees celcius.");
+            resultText.innerHTML=(summary);
             break;
     
             case 'snow':
+            imgHome.src="images/snowy.svg";
+            tempLocation.innerHTML=("The temperature is " + tempF + " degrees celcius.");
+            resultText.innerHTML=(summary);
             break;
     
             case 'sleet':
+            imgHome.src="images/sleet.svg";
+            tempLocation.innerHTML=("The temperature is " + tempF + " degrees celcius.");
+            resultText.innerHTML=(summary);
             break;
     
             case 'wind':
+            imgHome.src="images/wind.png";
+            tempLocation.innerHTML=("The temperature is " + tempF + " degrees celcius.");
+            resultText.innerHTML=(summary);
             break;
     
             case 'fog':
+            imgHome.src="images/fog.svg";
+            tempLocation.innerHTML=("The temperature is " + tempF + " degrees celcius.");
+            resultText.innerHTML=(summary);
             break;
     
             case 'cloudy':
+            imgHome.src="images/cloudy2.svg";
+            tempLocation.innerHTML=("The temperature is " + tempF + " degrees celcius.");
+            resultText.innerHTML=(summary);
             break;
     
             case 'partly-cloudy-day':
-            imgHome.src="images/sun.svg"
-            console.log("case");
+            imgHome.src="images/partlycloud.svg";
+            tempLocation.innerHTML=("The temperature is " + tempF + " degrees celcius.");
+            resultText.innerHTML=(summary);
             break;
     
             case 'partly-cloudy-night':
+            imgHome.src="images/cloudnight.svg";
+            tempLocation.innerHTML=("The temperature is " + tempF + " degrees celcius.");
+            resultText.innerHTML=(summary);
             break;
-    
+            
             default:
+            imgHome.src="images/search.png";
+            tempLocation.innerHTML=("The temperature is " + tempF + " degrees celcius.");
+            resultText.innerHTML=("We had trouble understanding the weather.");
           }
-      
-      
-
     })}
 
     function error() {
@@ -72,6 +103,5 @@ function weather() {
     location.innerHTML = "Locating...";
   }
   
-
-  weather();
-  //iconSelect();
+//call main function  
+weather();
